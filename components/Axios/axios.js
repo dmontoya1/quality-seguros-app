831ad618d02a9525ef0da2f2adfb1909031860e3
Diego Alejandro Castaño Gómez <dcastano@apptitud.com.co>
2018-12-10 13:18:22 -0500

    Updating iOS notifications

--- components/Axios/axios.js
+++ components/Axios/axios.js
@@ -1,8 +1,8 @@
 import * as axios from 'axios';
 
 const instance = axios.create();
-// instance.defaults.baseURL = 'http://138.197.86.31:10000/';
-instance.defaults.baseURL = 'http://192.168.0.22:8000/';
+instance.defaults.baseURL = 'http://138.197.86.31:10000/';
+// instance.defaults.baseURL = 'http://192.168.0.22:8000/';
 
 
 export { instance as default };
