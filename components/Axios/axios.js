fd91cb9c1ba9398423380b229c0f9493b948f34a
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-12-21 09:16:13 -0500

    Updating Axios

--- components/Axios/axios.js
+++ components/Axios/axios.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios';
 
 const instance = axios.create();
-instance.defaults.baseURL = 'http://stage.apptitud.com.co:10000/';
+instance.defaults.baseURL = 'http://misegurovirtual.com/';
 // instance.defaults.baseURL = 'http://192.168.0.6:8000/';
 
 
